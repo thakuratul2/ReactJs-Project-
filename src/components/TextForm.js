3b9d02e630343010420b4e4c9c7f123a68fd5118
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-05 00:28:00 +0530

    Enable Dark Mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,39 +1,105 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-    const changeUpUpper = ()=>{ //arrow function
-     let newText = text.toUpperCase();
-     setText(newText);
-    }
-    const changeText = (event)=>{
-        setText(event.target.value);
-    }
-function changeUpLower(){
+  const [myStyle, setMyStyle] = useState({
+    color: "black",
+    backgroundColor: "white",
+  });
+  const [btnText, setBtn] = useState("Enable Dark Mode");
+  const changeUpUpper = () => {
+    //arrow function
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const changeText = (event) => {
+    setText(event.target.value);
+  };
+  function changeUpLower() {
     let newText = text.toLowerCase();
     setText(newText);
-}
-const changeClearData=()=>{
-    let clearText = '';
+  }
+   const removeSpace = ()=>{
+    let removeText = text.split(/[ ]+/);
+    setText(removeText.join(" "));
+   }
+  const changeMode = () => {
+    if (myStyle.color === "black" && myStyle.backgroundColor === "white") {
+      setMyStyle({
+        color: "white",
+        backgroundColor: "black",
+      });
+      setBtn("Enable Light Mode");
+    } else {
+      setMyStyle({
+        color: "black",
+        backgroundColor: "white",
+      });
+      setBtn("Enable Dark Mode");
+    }
+  };
+  const changeClearData = () => {
+    let clearText = "";
     setText(clearText);
-}
-    const [text,setText] = useState("") //hook of useState
+  };
+
+  const [text, setText] = useState(""); //hook of useState
   return (
     <>
-    <div className="container mb-3">
-    <h2>{props.heading}</h2>
-  <textarea className="form-control" id="myBox" value={text} onChange={changeText} rows="3"></textarea>
-  <button className='btn btn-primary my-5 mx-1' onClick={changeUpUpper}>Change to UpperCase</button>
-  <button className='btn btn-primary my-5 mx-1' onClick={changeUpLower}>Change to LowerCase</button>
-  <button className='btn btn-primary my-5 mx-1' onClick={changeClearData}>Clear</button>
-
-</div>
-<div className="container">
-    <h2>Total Words & Character</h2>
-    <p>{text.split('').length} Words & {text.length} Character</p>
-    <p>{0.008 * text.split('').length} Mintues take to read the text</p>
-    <h3>Preview</h3>
-    <p>{text}</p>
-</div>
-</>
+      <div className="container mb-3" style={myStyle}>
+        <h2>{props.heading}</h2>
+        <textarea
+          className="form-control"
+          id="myBox"
+          value={text}
+          style={myStyle}
+          onChange={changeText}
+          rows="3"
+        ></textarea>
+        <button
+          className="btn btn-primary my-5 mx-1"
+          onClick={changeUpUpper}
+          style={myStyle}
+        >
+          Change to UpperCase
+        </button>
+        <button
+          className="btn btn-primary my-5 mx-1"
+          onClick={changeUpLower}
+          style={myStyle}
+        >
+          Change to LowerCase
+        </button>
+        <button
+          className="btn btn-primary my-5 mx-1"
+          onClick={removeSpace}
+          style={myStyle}
+        >
+          Remove Spaces
+        </button>
+        <button
+          className="btn btn-primary my-5 mx-1"
+          onClick={changeMode}
+          style={myStyle}
+        >
+          {btnText}
+        </button>
+        <button
+          className="btn btn-primary my-5 mx-1"
+          onClick={changeClearData}
+          style={myStyle}
+        >
+          Clear
+        </button>
+      </div>
+      <div className="container" style={myStyle}>
+        <h2>Total Words & Character</h2>
+        <p>
+          {text.split("").length} Words & {text.length} Character
+        </p>
+        <p>{0.008 * text.split("").length} Mintues take to read the text</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
