832b321414e90df1fbbf1a6a475319dc323804e9
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-04 22:10:52 +0530

    Add More

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,6 +12,10 @@ function changeUpLower(){
     let newText = text.toLowerCase();
     setText(newText);
 }
+const changeClearData=()=>{
+    let clearText = '';
+    setText(clearText);
+}
     const [text,setText] = useState("") //hook of useState
   return (
     <>
@@ -20,10 +24,13 @@ function changeUpLower(){
   <textarea className="form-control" id="myBox" value={text} onChange={changeText} rows="3"></textarea>
   <button className='btn btn-primary my-5 mx-1' onClick={changeUpUpper}>Change to UpperCase</button>
   <button className='btn btn-primary my-5 mx-1' onClick={changeUpLower}>Change to LowerCase</button>
+  <button className='btn btn-primary my-5 mx-1' onClick={changeClearData}>Clear</button>
+
 </div>
 <div className="container">
     <h2>Total Words & Character</h2>
     <p>{text.split('').length} Words & {text.length} Character</p>
+    <p>{0.008 * text.split('').length} Mintues take to read the text</p>
     <h3>Preview</h3>
     <p>{text}</p>
 </div>
