d831abc452f856503bd03f4bd36739679ab42923
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-04 22:04:00 +0530

    Adding More

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,14 +12,21 @@ function changeUpLower(){
     let newText = text.toLowerCase();
     setText(newText);
 }
-    const [text,setText] = useState("Enter text here") //hook of useState
+    const [text,setText] = useState("") //hook of useState
   return (
-    <div className="mb-3">
+    <>
+    <div className="container mb-3">
     <h2>{props.heading}</h2>
   <textarea className="form-control" id="myBox" value={text} onChange={changeText} rows="3"></textarea>
-  <button className='btn btn-primary my-5' onClick={changeUpUpper}>Change to UpperCase</button>
-  <button className='btn btn-primary my-5' onClick={changeUpLower}>Change to UpperCase</button>
+  <button className='btn btn-primary my-5 mx-1' onClick={changeUpUpper}>Change to UpperCase</button>
+  <button className='btn btn-primary my-5 mx-1' onClick={changeUpLower}>Change to LowerCase</button>
 </div>
-
+<div className="container">
+    <h2>Total Words & Character</h2>
+    <p>{text.split('').length} Words & {text.length} Character</p>
+    <h3>Preview</h3>
+    <p>{text}</p>
+</div>
+</>
   );
 }
